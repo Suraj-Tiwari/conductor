3956ddb075a02bcebf89b76cc9259503edd8aa75
rajkumar <rajkumar>
2021-04-28 17:12:05 +0530

    updated auth dynamically

--- es6-persistence/src/main/java/com/netflix/conductor/es6/config/ElasticSearchProperties.java
+++ es6-persistence/src/main/java/com/netflix/conductor/es6/config/ElasticSearchProperties.java
@@ -26,197 +26,227 @@ import java.util.stream.Collectors;
 @ConfigurationProperties("conductor.elasticsearch")
 public class ElasticSearchProperties {
 
-    /**
-     * The comma separated list of urls for the elasticsearch cluster. Format -- host1:port1,host2:port2
-     */
-    private String url = "localhost:9300";
+	/**
+	 * The comma separated list of urls for the elasticsearch cluster. Format --
+	 * host1:port1,host2:port2
+	 */
+	private String url = "localhost:9300";
 
-    /**
-     * The index prefix to be used when creating indices
-     */
-    private String indexPrefix = "conductor";
+	/**
+	 * The username for the elasticsearch cluster
+	 */
+	private String username = "";
 
-    /**
-     * The color of the elasticserach cluster to wait for to confirm healthy status
-     */
-    private String clusterHealthColor = "green";
+	/**
+	 * The username for the elasticsearch password
+	 */
+	private String password = "";
 
-    /**
-     * The size of the batch to be used for bulk indexing in async mode
-     */
-    private int indexBatchSize = 1;
+	/**
+	 * The index prefix to be used when creating indices
+	 */
+	private String indexPrefix = "conductor";
 
-    /**
-     * The size of the queue used for holding async indexing tasks
-     */
-    private int asyncWorkerQueueSize = 100;
+	/**
+	 * The color of the elasticserach cluster to wait for to confirm healthy status
+	 */
+	private String clusterHealthColor = "green";
 
-    /**
-     * The maximum number of threads allowed in the async pool
-     */
-    private int asyncMaxPoolSize = 12;
+	/**
+	 * The size of the batch to be used for bulk indexing in async mode
+	 */
+	private int indexBatchSize = 1;
 
-    /**
-     * The time in seconds after which the async buffers will be flushed (if no activity) to prevent data loss
-     */
-    @DurationUnit(ChronoUnit.SECONDS)
-    private Duration asyncBufferFlushTimeout = Duration.ofSeconds(10);
+	/**
+	 * The size of the queue used for holding async indexing tasks
+	 */
+	private int asyncWorkerQueueSize = 100;
 
-    /**
-     * The number of shards that the index will be created with
-     */
-    private int indexShardCount = 5;
+	/**
+	 * The maximum number of threads allowed in the async pool
+	 */
+	private int asyncMaxPoolSize = 12;
 
-    /**
-     * The number of replicas that the index will be configured to have
-     */
-    private int indexReplicasCount = 1;
+	/**
+	 * The time in seconds after which the async buffers will be flushed (if no
+	 * activity) to prevent data loss
+	 */
+	@DurationUnit(ChronoUnit.SECONDS)
+	private Duration asyncBufferFlushTimeout = Duration.ofSeconds(10);
 
-    /**
-     * The number of task log results that will be returned in the response
-     */
-    private int taskLogResultLimit = 10;
+	/**
+	 * The number of shards that the index will be created with
+	 */
+	private int indexShardCount = 5;
 
-    /**
-     * The timeout in milliseconds used when requesting a connection from the connection manager
-     */
-    private int restClientConnectionRequestTimeout = -1;
+	/**
+	 * The number of replicas that the index will be configured to have
+	 */
+	private int indexReplicasCount = 1;
 
-    /**
-     * Used to control if index management is to be enabled or will be controlled externally
-     */
-    private boolean autoIndexManagementEnabled = true;
+	/**
+	 * The number of task log results that will be returned in the response
+	 */
+	private int taskLogResultLimit = 10;
 
-    /**
-     * Document types are deprecated in ES6 and removed from ES7. This property can be used to disable the use of
-     * specific document types with an override. This property is currently used in ES6 module.
-     * <p>
-     * <em>Note that this property will only take effect if
-     * {@link ElasticSearchProperties#isAutoIndexManagementEnabled} is set to false and index management is handled
-     * outside of this module.</em>
-     */
-    private String documentTypeOverride = "";
+	/**
+	 * The timeout in milliseconds used when requesting a connection from the
+	 * connection manager
+	 */
+	private int restClientConnectionRequestTimeout = -1;
 
-    public String getUrl() {
-        return url;
-    }
+	/**
+	 * Used to control if index management is to be enabled or will be controlled
+	 * externally
+	 */
+	private boolean autoIndexManagementEnabled = true;
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
+	/**
+	 * Document types are deprecated in ES6 and removed from ES7. This property can
+	 * be used to disable the use of specific document types with an override. This
+	 * property is currently used in ES6 module.
+	 * <p>
+	 * <em>Note that this property will only take effect if
+	 * {@link ElasticSearchProperties#isAutoIndexManagementEnabled} is set to false
+	 * and index management is handled outside of this module.</em>
+	 */
+	private String documentTypeOverride = "";
 
-    public String getIndexPrefix() {
-        return indexPrefix;
-    }
+	public String getUrl() {
+		return url;
+	}
 
-    public void setIndexPrefix(String indexPrefix) {
-        this.indexPrefix = indexPrefix;
-    }
+	public void setUrl(String url) {
+		this.url = url;
+	}
 
-    public String getClusterHealthColor() {
-        return clusterHealthColor;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setClusterHealthColor(String clusterHealthColor) {
-        this.clusterHealthColor = clusterHealthColor;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public int getIndexBatchSize() {
-        return indexBatchSize;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setIndexBatchSize(int indexBatchSize) {
-        this.indexBatchSize = indexBatchSize;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public int getAsyncWorkerQueueSize() {
-        return asyncWorkerQueueSize;
-    }
+	public String getIndexPrefix() {
+		return indexPrefix;
+	}
 
-    public void setAsyncWorkerQueueSize(int asyncWorkerQueueSize) {
-        this.asyncWorkerQueueSize = asyncWorkerQueueSize;
-    }
+	public void setIndexPrefix(String indexPrefix) {
+		this.indexPrefix = indexPrefix;
+	}
 
-    public int getAsyncMaxPoolSize() {
-        return asyncMaxPoolSize;
-    }
+	public String getClusterHealthColor() {
+		return clusterHealthColor;
+	}
 
-    public void setAsyncMaxPoolSize(int asyncMaxPoolSize) {
-        this.asyncMaxPoolSize = asyncMaxPoolSize;
-    }
+	public void setClusterHealthColor(String clusterHealthColor) {
+		this.clusterHealthColor = clusterHealthColor;
+	}
 
-    public Duration getAsyncBufferFlushTimeout() {
-        return asyncBufferFlushTimeout;
-    }
+	public int getIndexBatchSize() {
+		return indexBatchSize;
+	}
 
-    public void setAsyncBufferFlushTimeout(Duration asyncBufferFlushTimeout) {
-        this.asyncBufferFlushTimeout = asyncBufferFlushTimeout;
-    }
+	public void setIndexBatchSize(int indexBatchSize) {
+		this.indexBatchSize = indexBatchSize;
+	}
 
-    public int getIndexShardCount() {
-        return indexShardCount;
-    }
+	public int getAsyncWorkerQueueSize() {
+		return asyncWorkerQueueSize;
+	}
 
-    public void setIndexShardCount(int indexShardCount) {
-        this.indexShardCount = indexShardCount;
-    }
+	public void setAsyncWorkerQueueSize(int asyncWorkerQueueSize) {
+		this.asyncWorkerQueueSize = asyncWorkerQueueSize;
+	}
 
-    public int getIndexReplicasCount() {
-        return indexReplicasCount;
-    }
+	public int getAsyncMaxPoolSize() {
+		return asyncMaxPoolSize;
+	}
 
-    public void setIndexReplicasCount(int indexReplicasCount) {
-        this.indexReplicasCount = indexReplicasCount;
-    }
+	public void setAsyncMaxPoolSize(int asyncMaxPoolSize) {
+		this.asyncMaxPoolSize = asyncMaxPoolSize;
+	}
 
-    public int getTaskLogResultLimit() {
-        return taskLogResultLimit;
-    }
+	public Duration getAsyncBufferFlushTimeout() {
+		return asyncBufferFlushTimeout;
+	}
 
-    public void setTaskLogResultLimit(int taskLogResultLimit) {
-        this.taskLogResultLimit = taskLogResultLimit;
-    }
+	public void setAsyncBufferFlushTimeout(Duration asyncBufferFlushTimeout) {
+		this.asyncBufferFlushTimeout = asyncBufferFlushTimeout;
+	}
 
-    public int getRestClientConnectionRequestTimeout() {
-        return restClientConnectionRequestTimeout;
-    }
+	public int getIndexShardCount() {
+		return indexShardCount;
+	}
 
-    public void setRestClientConnectionRequestTimeout(int restClientConnectionRequestTimeout) {
-        this.restClientConnectionRequestTimeout = restClientConnectionRequestTimeout;
-    }
+	public void setIndexShardCount(int indexShardCount) {
+		this.indexShardCount = indexShardCount;
+	}
 
-    public boolean isAutoIndexManagementEnabled() {
-        return autoIndexManagementEnabled;
-    }
+	public int getIndexReplicasCount() {
+		return indexReplicasCount;
+	}
 
-    public void setAutoIndexManagementEnabled(boolean autoIndexManagementEnabled) {
-        this.autoIndexManagementEnabled = autoIndexManagementEnabled;
-    }
+	public void setIndexReplicasCount(int indexReplicasCount) {
+		this.indexReplicasCount = indexReplicasCount;
+	}
 
-    public String getDocumentTypeOverride() {
-        return documentTypeOverride;
-    }
+	public int getTaskLogResultLimit() {
+		return taskLogResultLimit;
+	}
 
-    public void setDocumentTypeOverride(String documentTypeOverride) {
-        this.documentTypeOverride = documentTypeOverride;
-    }
+	public void setTaskLogResultLimit(int taskLogResultLimit) {
+		this.taskLogResultLimit = taskLogResultLimit;
+	}
 
-    public List<URL> toURLs() {
-        String clusterAddress = getUrl();
-        String[] hosts = clusterAddress.split(",");
-        return Arrays.stream(hosts)
-            .map(host ->
-                (host.startsWith("http://") || host.startsWith("https://") || host.startsWith("tcp://"))
-                    ? toURL(host)
-                    : toURL("tcp://" + host)
-            ).collect(Collectors.toList());
-    }
+	public int getRestClientConnectionRequestTimeout() {
+		return restClientConnectionRequestTimeout;
+	}
 
-    private URL toURL(String url) {
-        try {
-            return new URL(url);
-        } catch (MalformedURLException e) {
-            throw new IllegalArgumentException(url + "can not be converted to java.net.URL");
-        }
-    }
+	public void setRestClientConnectionRequestTimeout(int restClientConnectionRequestTimeout) {
+		this.restClientConnectionRequestTimeout = restClientConnectionRequestTimeout;
+	}
+
+	public boolean isAutoIndexManagementEnabled() {
+		return autoIndexManagementEnabled;
+	}
+
+	public void setAutoIndexManagementEnabled(boolean autoIndexManagementEnabled) {
+		this.autoIndexManagementEnabled = autoIndexManagementEnabled;
+	}
+
+	public String getDocumentTypeOverride() {
+		return documentTypeOverride;
+	}
+
+	public void setDocumentTypeOverride(String documentTypeOverride) {
+		this.documentTypeOverride = documentTypeOverride;
+	}
+
+	public List<URL> toURLs() {
+		String clusterAddress = getUrl();
+		String[] hosts = clusterAddress.split(",");
+		return Arrays.stream(hosts)
+				.map(host -> (host.startsWith("http://") || host.startsWith("https://") || host.startsWith("tcp://"))
+						? toURL(host)
+						: toURL("tcp://" + host))
+				.collect(Collectors.toList());
+	}
+
+	private URL toURL(String url) {
+		try {
+			return new URL(url);
+		} catch (MalformedURLException e) {
+			throw new IllegalArgumentException(url + "can not be converted to java.net.URL");
+		}
+	}
 }
--- es6-persistence/src/main/java/com/netflix/conductor/es6/config/ElasticSearchV6Configuration.java
+++ es6-persistence/src/main/java/com/netflix/conductor/es6/config/ElasticSearchV6Configuration.java
@@ -47,89 +47,83 @@ import java.util.Optional;
 @Conditional(ElasticSearchConditions.ElasticSearchV6Enabled.class)
 public class ElasticSearchV6Configuration {
 
-    private static final Logger log = LoggerFactory.getLogger(ElasticSearchV6Configuration.class);
+	private static final Logger log = LoggerFactory.getLogger(ElasticSearchV6Configuration.class);
 
-    @Bean
-    public Client client(ElasticSearchProperties properties) {
-        Settings.Builder settingsBuilder = Settings.builder()
-                .put("client.transport.ignore_cluster_name", true)
-                .put("client.transport.sniff", true);
-//
-//                .build();
-        if (true) { //TODO: check if auth exist
-            settingsBuilder.put("xpack.security.user", "username:password"); //TODO: update user:password from dynamic param
-        }
-        Settings settings = settingsBuilder.build();
+	@Bean
+	public Client client(ElasticSearchProperties properties) {
+		Settings settings = Settings.builder().put("client.transport.ignore_cluster_name", true)
+				.put("client.transport.sniff", true).build();
+		TransportClient transportClient = new PreBuiltTransportClient(settings);
 
-        TransportClient transportClient = new PreBuiltTransportClient(settings);
+		List<URL> clusterAddresses = properties.toURLs();
 
-        List<URL> clusterAddresses = properties.toURLs();
+		if (clusterAddresses.isEmpty()) {
+			log.warn("workflow.elasticsearch.url is not set.  Indexing will remain DISABLED.");
+		}
+		for (URL hostAddress : clusterAddresses) {
+			int port = Optional.ofNullable(hostAddress.getPort()).orElse(9200);
+			try {
+				transportClient
+						.addTransportAddress(new TransportAddress(InetAddress.getByName(hostAddress.getHost()), port));
+			} catch (Exception e) {
+				throw new RuntimeException("Invalid host" + hostAddress.getHost(), e);
+			}
+		}
+		return transportClient;
+	}
 
-        if (clusterAddresses.isEmpty()) {
-            log.warn("workflow.elasticsearch.url is not set.  Indexing will remain DISABLED.");
-        }
-        for (URL hostAddress : clusterAddresses) {
-            int port = Optional.ofNullable(hostAddress.getPort()).orElse(9200);
-            try {
-                transportClient
-                        .addTransportAddress(new TransportAddress(InetAddress.getByName(hostAddress.getHost()), port));
-            } catch (Exception e) {
-                throw new RuntimeException("Invalid host" + hostAddress.getHost(), e);
-            }
-        }
-        return transportClient;
-    }
+	@Bean
+	public RestClient restClient(ElasticSearchProperties properties) {
+		RestClientBuilder restClientBuilder = RestClient.builder(convertToHttpHosts(properties.toURLs()));
+		if (properties.getRestClientConnectionRequestTimeout() > 0) {
+			restClientBuilder.setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder
+					.setConnectionRequestTimeout(properties.getRestClientConnectionRequestTimeout()));
+		}
+		if (true) { // TODO: check if auth exist
+			final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+			credentialsProvider.setCredentials(AuthScope.ANY,
+					new UsernamePasswordCredentials(properties.getUsername(), properties.getPassword()));
+			restClientBuilder.setHttpClientConfigCallback(new HttpClientConfigCallback() {
+				@Override
+				public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+					return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+				}
+			});
+		}
+		return restClientBuilder.build();
+	}
 
-    @Bean
-    public RestClient restClient(ElasticSearchProperties properties) {
-        RestClientBuilder restClientBuilder = RestClient.builder(convertToHttpHosts(properties.toURLs()));
-        if (properties.getRestClientConnectionRequestTimeout() > 0) {
-            restClientBuilder.setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder
-                    .setConnectionRequestTimeout(properties.getRestClientConnectionRequestTimeout()));
-        }
-        if (true) { // TODO: check if auth exist
-            final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-            credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("user", "test-user-password")); //TODO: make dynamic
-            restClientBuilder.setHttpClientConfigCallback(new HttpClientConfigCallback() {
-                @Override
-                public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
-                    return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
-                }
-            });
-        }
-        return restClientBuilder.build();
-    }
+	@Bean
+	public RestClientBuilder restClientBuilder(ElasticSearchProperties properties) {
+		RestClientBuilder builder = RestClient.builder(convertToHttpHosts(properties.toURLs()));
 
-    @Bean
-    public RestClientBuilder restClientBuilder(ElasticSearchProperties properties) {
-        RestClientBuilder builder = RestClient.builder(convertToHttpHosts(properties.toURLs()));
+		if (true) { // TODO: check if auth exist
+			final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+			credentialsProvider.setCredentials(AuthScope.ANY,
+					new UsernamePasswordCredentials(properties.getUsername(), properties.getPassword())); 																											// dynamic
+			builder.setHttpClientConfigCallback(new HttpClientConfigCallback() {
+				@Override
+				public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+					return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+				}
+			});
+		}
+		return builder;
+	}
 
-        if (true) { // TODO: check if auth exist
-            final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-            credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("user", "test-user-password")); //TODO: make dynamic
-            builder.setHttpClientConfigCallback(new HttpClientConfigCallback() {
-                @Override
-                public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
-                    return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
-                }
-            });
-        }
-        return builder;
-    }
+	@Bean
+	public IndexDAO es6IndexDAO(RestClientBuilder restClientBuilder, Client client, ElasticSearchProperties properties,
+			ObjectMapper objectMapper) {
+		String url = properties.getUrl();
+		if (url.startsWith("http") || url.startsWith("https")) {
+			return new ElasticSearchRestDAOV6(restClientBuilder, properties, objectMapper);
+		} else {
+			return new ElasticSearchDAOV6(client, properties, objectMapper);
+		}
+	}
 
-    @Bean
-    public IndexDAO es6IndexDAO(RestClientBuilder restClientBuilder, Client client, ElasticSearchProperties properties,
-                                ObjectMapper objectMapper) {
-        String url = properties.getUrl();
-        if (url.startsWith("http") || url.startsWith("https")) {
-            return new ElasticSearchRestDAOV6(restClientBuilder, properties, objectMapper);
-        } else {
-            return new ElasticSearchDAOV6(client, properties, objectMapper);
-        }
-    }
-
-    private HttpHost[] convertToHttpHosts(List<URL> hosts) {
-        return hosts.stream()
-                .map(host -> new HttpHost(host.getHost(), host.getPort(), host.getProtocol())).toArray(HttpHost[]::new);
-    }
+	private HttpHost[] convertToHttpHosts(List<URL> hosts) {
+		return hosts.stream().map(host -> new HttpHost(host.getHost(), host.getPort(), host.getProtocol()))
+				.toArray(HttpHost[]::new);
+	}
 }
